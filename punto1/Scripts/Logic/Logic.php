85b618b339371b89eaf2363e9ca8d763cf837f98
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-06-03 21:03:44 -0500

    feat: :sparkles: Se termina el punto con manejo de errores de que el usario pueda poner

--- punto1/Scripts/Logic/Logic.php
+++ punto1/Scripts/Logic/Logic.php
@@ -1,11 +1,20 @@
 <?php
-$planetas=[array("nombre"=>"Sol","Habitado"=>"Deshabitado"),"Mercurio","Venus","Tierra","Marte","Jupiter","Saturno","Urano","Neptuno"];
+$planetas=[array("nombre"=>"sol","Habitado"=>"Deshabitado"),array("nombre"=>"mercurio","Habitado"=>"Deshabitado"),array("nombre"=>"venus","Habitado"=>"Deshabitado"),array("nombre"=>"tierra","Habitado"=>"Deshabitado"),array("nombre"=>"marte","Habitado"=>"Deshabitado"),array("nombre"=>"jupiter","Habitado"=>"Deshabitado"),array("nombre"=>"saturno","Habitado"=>"Deshabitado"),array("nombre"=>"urano","Habitado"=>"Deshabitado"),array("nombre"=>"neptuno","Habitado"=>"Deshabitado")];
 
 if (is_numeric($_POST["Planet"]) ){
-    $temp_toReturn=$planetas[intval($_POST["Planet"])];
+    $temp_toReturn=$planetas[intval($_POST["Planet"])]["nombre"];
     if ($temp_toReturn){
         header("location:../Views/Resultado.php?element=".$temp_toReturn."&id=".intval($_POST["Planet"]));
     }else{
         header("location:../Views/BadRequest.php");
     }
-}else{}
+}elseif(is_numeric($_POST["Planet"]) == false){
+    foreach ($planetas as $key => $planeta) {
+        if ($planeta["nombre"] === strtolower($_POST["Planet"])) {
+            header("location:../Views/Resultado.php?element=".$planeta["nombre"]."&id=".$key);
+        }else{
+            header("location:../Views/BadRequest.php");
+        }
+    }
+
+}
